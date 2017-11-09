da69a2ce0fff3d235a5ba7fcd6e7cac4dce0c4a6
Sonal Ranjit <sonal.ranjit3@gmail.com>
2017-11-08 22:48:25 -0500

    added APIManager for models

--- app/__init__.py
+++ app/__init__.py
@@ -4,9 +4,27 @@ from flask_sqlalchemy import SQLAlchemy
 
 #Initialize the app
 app = Flask(__name__, instance_relative_config=True)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'ADD_THE_AWS_PG_CONNECTION_STRING_HERE'
+db = SQLAlchemy(app)
+#load the config file
+#app.config.from_object('config')
 
 #Load the views
 from app import views
+from app.models import *
 
-#load the config file
-app.config.from_object('config')
+#Create the Flask-Restless API Manager
+manager = APIManager(app, flask_sqlalchemy_db=db)
+
+#All the endpoints can be accessed under /api/[tablename]
+manager.create_api(UniversityEmploymentRates, methods=['GET'])
+manager.create_api(UniversityProgramList, methods=['GET'])
+manager.create_api(NOCSynonymList, methods=['GET'])
+manager.create_api(NOCList, methods=['GET'])
+manager.create_api(SkillLevelList, methods=['GET'])
+manager.create_api(TaskList, methods=['GET'])
+manager.create_api(EssentialSkills, methods=['GET'])
+manager.create_api(TotalJobOpenings, methods=['GET'])
+manager.create_api(NOCLocationList, methods=['GET'])
+manager.create_api(NOCSalaryList, methods=['GET'])
